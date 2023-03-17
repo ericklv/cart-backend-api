88c6c610ebb8b779ba5e0ea2bb08e786ecfaa577
elazon <elazon01@gmail.com>
2023-03-17 02:56:18 -0500

    add interface for service

--- src/main/java/com/xcale/demo2/sampledemo/service/CartService.java
+++ src/main/java/com/xcale/demo2/sampledemo/service/CartService.java
@@ -1,61 +1,18 @@
 package com.xcale.demo2.sampledemo.service;
 
 import com.xcale.demo2.sampledemo.model.Cart;
-import com.xcale.demo2.sampledemo.model.CartItem;
-import com.xcale.demo2.sampledemo.model.Item;
-import com.xcale.demo2.sampledemo.repository.CartRepository;
-import com.xcale.demo2.sampledemo.repository.ItemRepository;
 import com.xcale.demo2.sampledemo.utils.exception.CustomException;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
-@Slf4j
-@Service
-public class CartService {
+public interface CartService {
+    Cart getOrder(Long id);
 
-    @Autowired
-    CartRepository cartRepository;
+    List<Cart> getAllCarts();
 
-    @Autowired
-    ItemRepository itemRepository;
+    Cart addCart(Cart cart) throws CustomException;
 
-    public Cart getOrder(Long id){
-        return cartRepository
-                .findById(id)
-                .orElse(null);
-    }
+    void delete(Long id);
 
-    public List<Cart> getAllCarts(){
-        return StreamSupport.stream(cartRepository.findAll().spliterator(),false)
-                .collect(Collectors.toList());
-    }
 
-    public Cart addCart(Cart cart) throws CustomException {
-        try {
-            cart.getCartItemList().stream().forEach(x -> addCartItems(x));
-            cart.getAmount();
-            cart.setCreatedAt(new Date());
-            return cartRepository.save(cart);
-        }catch(Exception e){
-            log.error(e.getMessage());
-            throw new CustomException("Cannot save cart");
-        }
-
-    }
-
-    public void delete(Long id){
-        cartRepository.deleteById(id);
-    }
-
-    private void addCartItems(CartItem x){
-        Item item = itemRepository.findById(x.getItem().getId()).get();
-        x.setPrice(item.getPrice());
-        x.setItem(item);
-    }
 }
--- src/main/java/com/xcale/demo2/sampledemo/service/ItemService.java
+++ src/main/java/com/xcale/demo2/sampledemo/service/ItemService.java
@@ -1,55 +1,21 @@
 package com.xcale.demo2.sampledemo.service;
 
 import com.xcale.demo2.sampledemo.model.Item;
-import com.xcale.demo2.sampledemo.repository.ItemRepository;
 import com.xcale.demo2.sampledemo.utils.exception.CustomException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
-@Service
-public class ItemService {
+public interface ItemService {
 
-    @Autowired
-    ItemRepository itemRepository;
+    Item getItemById(Long id);
 
-    public Item getItemById(Long id){
-        return itemRepository
-                .findById(id)
-                .orElse(null);
-    }
+    List<Item> getAll();
 
-    public List<Item> getAll(){
-        return StreamSupport.stream(itemRepository.findAll().spliterator(),false)
-                .collect(Collectors.toList());
-    }
+    Item addItem(Item item);
 
-    public Item addItem(Item item){
-        return itemRepository.save(item);
-    }
+    Item update(Long id, Item item) throws CustomException;
 
-    public Item update(Long id, Item item) throws CustomException {
-        Item itemUpdated = itemRepository.findById(id).orElseThrow(() -> new CustomException("Not found"));
-        itemUpdated.setName(item.getName());
-        itemUpdated.setPrice(item.getPrice());
-        itemUpdated.setDescription(item.getDescription());
-        itemUpdated.setItemType(item.getItemType());
-        itemUpdated.setImageUrl(item.getImageUrl());
+    Item updateImage(Long id, Item item) throws CustomException;
 
-        return itemRepository.save(itemUpdated);
-    }
-
-    public Item updateImage(Long id, Item item) throws CustomException {
-        Item itemUpdated = itemRepository.findById(id).orElseThrow(() -> new CustomException("Not found"));
-        itemUpdated.setDescription(item.getDescription());
-        itemUpdated.setImageUrl(item.getImageUrl());
-
-        return itemRepository.save(itemUpdated);
-    }
-
-    public void deleteItem(Long id){ itemRepository.deleteById(id);}
-
+    void deleteItem(Long id);
 }
